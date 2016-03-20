0046f107c1c16ae3ccec53fe60782032e466c530
tjcuwang@yahoo.com <tjcuwang@yahoo.com>
2016-03-20 20:32:45 +0800

    Create new Branch

---
+++ aedms-gui/app/scripts/services/apu_service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aedmsGuiApp.service:APUServie
+ * @description
+ * # EngineService
+ * Controller of the aedmsGuiApp
+ */
+angular.module('aedmsGuiApp')
+.factory('APUService', function ($resource) {
+    return $resource('http://localhost:8080/aedms-core/aPUs/:apu', {apu: "@apu"});
+});
