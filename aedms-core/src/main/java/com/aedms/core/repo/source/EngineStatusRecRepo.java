998777bfc6e9b86d5b79ac2e032383087e71f1ba
Michael Wang <tjcuwang@yahoo.com>
2016-06-14 22:07:37 +0800

    Change the restful path for engineopr and enginestat

--- aedms-core/src/main/java/com/aedms/core/repo/source/EngineStatusRecRepo.java
+++ aedms-core/src/main/java/com/aedms/core/repo/source/EngineStatusRecRepo.java
@@ -5,9 +5,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.aedms.core.entities.source.Engine;
 import com.aedms.core.entities.source.EngineOperationRec;
+import com.aedms.core.entities.source.EngineStatusRec;
 
-@RepositoryRestResource(collectionResourceRel = "engineoprs", path = "engineoprs")
-public interface EngineOprRepo extends CrudRepository<EngineOperationRec, Long> {
+@RepositoryRestResource(collectionResourceRel = "enginestatusrecs", path = "enginestatusrecs")
+public interface EngineStatusRecRepo extends CrudRepository<EngineStatusRec, Long> {
 
 
 }
