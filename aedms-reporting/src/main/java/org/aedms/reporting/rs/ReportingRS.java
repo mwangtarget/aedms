5a2871977e0e7531ffe793b5dc432da010b78bb6
Michael Wang <tjcuwang@yahoo.com>
2016-07-18 22:23:00 +0800

    Add the PDF streaming back to client

--- aedms-reporting/src/main/java/org/aedms/reporting/rs/ReportingRS.java
+++ aedms-reporting/src/main/java/org/aedms/reporting/rs/ReportingRS.java
@@ -1,5 +1,9 @@
 package org.aedms.reporting.rs;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import org.aedms.reporting.core.ReportRequest;
 import org.eclipse.birt.report.engine.api.EngineException;
 import org.eclipse.birt.report.engine.api.HTMLRenderOption;
@@ -11,6 +15,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,17 +36,18 @@ public class ReportingRS {
 	private Environment environment;
 	
 	@RequestMapping(value = "/reportgen", method = RequestMethod.POST)
-	public void reportGenerate(@RequestBody final ReportRequest reportRequest) {
+	public ResponseEntity<InputStreamResource>  reportGenerate(@RequestBody final ReportRequest reportRequest) {
 		
 		IReportRunnable reportDesign = null;
 		try {
-			reportDesign = reportEngine.openReportDesign(this.getClass().getResourceAsStream(environment.getRequiredProperty(reportRequest.getReportName())));
+			reportDesign = reportEngine.openReportDesign(new ClassPathResource(environment.getRequiredProperty(reportRequest.getReportName())).getInputStream());
             //	reportDesign = reportEngine.openReportDesign(reportRequest.getReportName());
 			
-		} catch (EngineException e) {
+		} catch (EngineException | IllegalStateException | IOException e) {
 			e.printStackTrace();
 			log.error("Fail to load the report design at server side: "+e.getMessage());
-			return;
+			//TODO: return a valid response
+			return null;
 		}
 
 		// Create task to run and render the report:
@@ -47,16 +56,28 @@ public class ReportingRS {
 		// Render option configuration
 		final PDFRenderOption pdfRenderOption = new PDFRenderOption();
         pdfRenderOption.setOutputFormat("pdf");
-        pdfRenderOption.setOutputFileName("cusomers.pdf");
+        ByteArrayOutputStream pdfOutputStream= new ByteArrayOutputStream();
+        pdfRenderOption.setOutputStream(pdfOutputStream);
+        // pdfRenderOption.setOutputFileName("tmp.pdf");
         reportTask.setRenderOption(pdfRenderOption);
  
         try {
 			reportTask.run();
+			return ResponseEntity
+            .ok()
+            .contentLength(pdfOutputStream.toByteArray().length)
+            .contentType(MediaType.parseMediaType("application/pdf"))
+            .body(new InputStreamResource(new ByteArrayInputStream(pdfOutputStream.toByteArray())));
+			
 		} catch (EngineException e) {
 			log.error("Fail to generate the report at server side: "+e.getMessage());
-			return;
+			//TODO: return a valid response
+			return null;
+		}finally{
+			reportTask.close();
 		}
-        reportTask.close();
+        
+        
 	}
 	
 	//TODO: remove it
